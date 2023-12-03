702753d34d1d69b9c03b689f6730d2c14330970e
hashrock <hashrock@users.noreply.github.com>
2023-12-03 22:41:14 +0900

    show files

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,19 @@
 import { defineRoute } from "$fresh/server.ts";
+import { walk } from "https://deno.land/std/fs/walk.ts";
 
 export default defineRoute(async (req, ctx) => {
   const cwd = Deno.cwd();
   const filePath = new URL(req.url, `file://${cwd}`).pathname;
   const importMeta = import.meta;
 
+  const root = await walk("/");
+  const rootFiles = [];
+  let i = 0;
+  for await (const file of root) {
+    rootFiles.push(file.name);
+    i++;
+    if (i > 100) break;
+  }
   return (
     <div class="page">
       <h1>Hello World</h1>
@@ -12,6 +21,11 @@ export default defineRoute(async (req, ctx) => {
       <p>Requested file path: {filePath}</p>
       <p>import.meta.url: {importMeta.url}</p>
       <p>import.meta.main: {importMeta.main}</p>
+
+      <h2>Root Files</h2>
+      <ul>
+        {rootFiles.map((file) => <li>{file}</li>)}
+      </ul>
     </div>
   );
 });
